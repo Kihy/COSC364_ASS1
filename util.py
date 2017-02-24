eb93b3a6a452385e4db2eb5c9900bbe7cc726842
Ke He <khe60@uclive.ac.nz>
2017-02-24 14:39:47 +1300

    improved reading

--- util.py
+++ util.py
@@ -1,10 +1,12 @@
+import sys
+
 def check_id(router_id):
     if router_id > 64000 or router_id<1:
-        return False
+        raise ValueError("Router ID out of bounds.")
 
 def check_port(port_num):
     if port_num <1024 or port_num>64000:
-        return False
+        raise ValueError("port number out of bounds.")
 
 def read_config(filename):
     router_id=None
@@ -15,20 +17,28 @@ def read_config(filename):
         line=line.rstrip()
         if line.startswith("//") or line=="":
             continue
+        try:
+            if line.startswith("router-id"):
+                router_id=int(line.split()[1])
 
-        if line.startswith("router-id"):
-            router_id=int(line.split()[1])
+            if line.startswith("input-ports"):
+                input_ports=list(map(int,line.split()[1].split(",")))
 
-        if line.startswith("input-ports"):
-            input_ports=list(map(int,line.split()[1].split(",")))
-
-        if line.startswith("outputs"):
-            output_port=line.split()[1].split(",")
-            for port in output_port:
-                output_ports.append(list(map(int,port.split("-"))))
-
-    print(router_id)
-    print(input_ports)
-    print(output_ports)
-
-read_config("conf1.txt")
+            if line.startswith("outputs"):
+                output_port=line.split()[1].split(",")
+                for port in output_port:
+                    output_ports.append(list(map(int,port.split("-"))))
+        except ValueError as e:
+            print(e.args[0])
+            sys.exit()
+        except IndexError as e:
+            print("missing inputs")
+            sys.exit()
+    try:
+        if router_id==None:
+            raise ValueError("Router ID not set")
+    except ValueError as e:
+        print(e.args[0])
+        sys.exit()
+    return router_id, input_ports, output_ports
+print(read_config("conf1.txt"))
