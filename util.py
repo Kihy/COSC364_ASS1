e9a6069904cdb5d1cdfd7f3f31520ce6b9766ce2
Ke He <khe60@uclive.ac.nz>
2017-02-27 13:32:27 +1300

    added packet to util

--- util.py
+++ util.py
@@ -1,4 +1,34 @@
 import sys
+import struct
+
+class Rip_packet:
+    """a rip v2 packet"""
+    def __init__(self, router_id):
+        self.command=2
+        self.version=2
+        self.router_id=router_id
+        self.entry_table=[]
+
+    def add_entry(self,address_family,route_tag,router_id,subnet_mask,next_hop,metric):
+        self.entry_table.append((address_family,route_tag,router_id,subnet_mask,next_hop,metric))
+
+    def dump(self):
+        #header
+        packet=struct.pack('BBH',self.command,self.version,self.router_id)
+        #add entry_table
+        for row in self.entry_table:
+            packet.append(struct.pack('HHIIII',row[0],row[1],row[2],row[3],row[4],row[5]))
+        return packet
+
+def load(self,packet):
+    num_entry=(len(packet)-4)/20
+    command,version,router_id=struct.unpack('BBH',packet[0:4])
+    rip=Rip_packet(router_id)
+    for i in range(num_entry):
+        address_family,route_tag,router_id,subnet_mask,next_hop,metric=struct.unpacket("HHIIII",packet[4+i*20:4+(i+1)*20])
+        rip.add_entry(address_family,route_tag,router_id,subnet_mask,next_hop,metric)
+    return rip
+
 
 def check_id(router_id):
     if router_id > 64000 or router_id<1:
