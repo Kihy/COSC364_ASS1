e710a203970331b3f9c5c700c5077eaf54812749
Ke He <khe60@uclive.ac.nz>
2017-02-28 10:16:28 +1300

    added packet clas

--- util.py
+++ util.py
@@ -1,6 +1,10 @@
 import sys
 import struct
 
+HEADER_FORMAT="BBH"
+ENTRY_FORMAT="HHIIII"
+LINE="-"*33+"\n"
+
 class Rip_packet:
     """a rip v2 packet"""
     def __init__(self, router_id):
@@ -9,26 +13,41 @@ class Rip_packet:
         self.router_id=router_id
         self.entry_table=[]
 
-    def add_entry(self,address_family,route_tag,router_id,subnet_mask,next_hop,metric):
-        self.entry_table.append((address_family,route_tag,router_id,subnet_mask,next_hop,metric))
+    def add_entry(self,entry):
+        self.entry_table.append(entry)
 
     def dump(self):
         #header
-        packet=struct.pack('BBH',self.command,self.version,self.router_id)
+        packet=struct.pack(HEADER_FORMAT,self.command,self.version,self.router_id)
         #add entry_table
         for row in self.entry_table:
-            packet.append(struct.pack('HHIIII',row[0],row[1],row[2],row[3],row[4],row[5]))
+            packet+=struct.pack(ENTRY_FORMAT,*row)
         return packet
 
-def load(self,packet):
-    num_entry=(len(packet)-4)/20
-    command,version,router_id=struct.unpack('BBH',packet[0:4])
+    def __str__(self):
+        string=LINE
+        string+="|{:^7}|{:^7}|{:^15}|\n".format(self.command,self.version,self.router_id)
+        string+=LINE
+        for entry in self.entry_table:
+            string+="|{:^15}|{:^15}|\n|{:^31}|\n|{:^31}|\n|{:^31}|\n|{:^31}|\n".format(*entry)
+            string+=LINE
+        return string
+
+def load(packet):
+    num_entry=(len(packet)-4)//20
+    command,version,router_id=struct.unpack(HEADER_FORMAT,packet[0:4])
     rip=Rip_packet(router_id)
     for i in range(num_entry):
-        address_family,route_tag,router_id,subnet_mask,next_hop,metric=struct.unpacket("HHIIII",packet[4+i*20:4+(i+1)*20])
-        rip.add_entry(address_family,route_tag,router_id,subnet_mask,next_hop,metric)
+        entry=struct.unpack(ENTRY_FORMAT,packet[4+i*20:4+(i+1)*20])
+        rip.add_entry(entry)
     return rip
-
+# # test packet load and dump functions
+# p=Rip_packet(1)
+# p.add_entry((1,1,1,1,1,1))
+# p.add_entry((2,2,2,2,2,2))
+# byte_stream=p.dump()
+# p2=load(byte_stream)
+# print(p2)
 
 def check_id(router_id):
     if router_id > 64000 or router_id<1:
@@ -71,4 +90,3 @@ def read_config(filename):
         print(e.args[0])
         sys.exit()
     return router_id, input_ports, output_ports
-print(read_config("conf1.txt"))
