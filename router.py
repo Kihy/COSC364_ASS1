b2e4b23092ecebdb54d81f77aca3c5f80de91eca
yzh231 <yzh231@uclive.ac.nz>
2017-04-15 16:08:01 +1200

    draft

--- router.py
+++ router.py
@@ -41,9 +41,9 @@ class Router(object):
     def add_output_port(self, output_port_number):
         self.output_port.append(output_port_number)
 
-    def add_routing_table(self, metric, dest_id, next_hop, timeout=0):
+    def add_routing_table(self, metric, dest_id, next_hop, timeout=0, firsttime=True):
         metric=min(INFINITY,metric)
-        self.routing_table[dest_id] = [metric, next_hop, timeout]
+        self.routing_table[dest_id] = [metric, next_hop, timeout,firsttime]
 
     def generate_packet(self, output_port):
         """generate a RIP packet from routing table"""
@@ -56,11 +56,12 @@ class Router(object):
         rip_packet.add_entry([0,0,self.router_id,0,self.router_id,0])
 
         for dest_id in self.routing_table.keys():
-            metric, next_hop, timeout = self.routing_table[dest_id]
+            metric, next_hop, timeout,_ = self.routing_table[dest_id]
 
-            if self.portDict[output_port] == next_hop:
-                if dest_id != next_hop:
-                    metric = INFINITY
+            if self.portDict[output_port] == next_hop:  #if neighbour is next_hop to dest, split horizon
+                if dest_id != next_hop:  #??
+                    metric = INFINITY   #poision reverse
+                    print("implemented split horizon with poision reverse, current router is",self.router_id,"next_hop is", next_hop,"dest is",dest_id )
             # print(metric, next_hop, timeout)
             entry = [0, 0, dest_id, 0, next_hop, metric]
 
@@ -99,15 +100,16 @@ class Router(object):
         self.periodic_update()
         self.update_Timer()
         self.disp()
+        lock= threading.Lock()
         while True:
             inputready, outputready, exceptrdy = select.select(
                 self.input_sockets, [], [], 0.5)
             for s in inputready:
+                lock.acquire()
                 data, addr = s.recvfrom(1024)
                 rip_packet = load(data)
 
-                if rip_packet.router_id not in self.portDict.values():  # If not a directly connected network
-                    break
+
                 print("packet loaded")
                 print(rip_packet)
 
@@ -115,10 +117,10 @@ class Router(object):
                     # print("Entry: " + str(entry))
                     command,version, dest, _, next_hop, entry_metric = entry
 
-                    if command == 2 and version == 2 and 1 <= entry_metric <= INFINITY:
+                    if command == 2 and version == 2 and 0<= entry_metric <= INFINITY:
                         continue
 
-                    # if entry is about it self and not inifinity, clear timeout for that router
+                    # skip if entry is about it self
                     if dest == self.router_id:
                         continue
 
@@ -128,9 +130,8 @@ class Router(object):
                         self.routing_table[rip_packet.router_id][0]
                     potential_metric = min(potential_metric, INFINITY)
                     # if not in routing table
-                    if dest not in self.routing_table.keys():
-                        self.add_routing_table(
-                            potential_metric, dest, rip_packet.router_id)
+                    if dest not in self.routing_table.keys() and potential_metric != INFINITY:
+                        self.add_routing_table(potential_metric, dest, rip_packet.router_id)
                     else:
                         # if next hop comes from the current neighbour
                         if self.routing_table[dest][1] == rip_packet.router_id:
@@ -138,22 +139,26 @@ class Router(object):
                             # update if values are different
                             if potential_metric != self.routing_table[dest][0]:
                                 self.routing_table[dest][0] = potential_metric
-                                self.routing_table[dest][
-                                    1] = rip_packet.router_id
+                                self.routing_table[dest][1] = rip_packet.router_id
 
                                 # if new metric is infinity trigger an update
                                 if potential_metric != INFINITY:
                                     #  reinitialize timer
                                     self.routing_table[dest][2] = 0
+                                    self.routing_table[dest][3] = True
                                 else:
-                                    self.send()
+
+                                    if self.routing_table[dest][3]:
+                                        self.send()
+                                        self.routing_table[dest][3]=False
+                                        self.routing_table[dest][2]=0
                             else:
                                 self.routing_table[dest][2] = 0
 
                         elif potential_metric < self.routing_table[dest][0]:
                             self.routing_table[dest] = [
                                 potential_metric, rip_packet.router_id, 0]
-
+                lock.release()
     def print_routing_table(self):
         print("Routing table")
         print("|{:^7}|{:^7}|{:^15}|{:^15}|".format(
